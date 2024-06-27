8ac950bfcea08ee97bf10f9d65f49987a033a3f3
Animesh Sinha <animeshsinha1903@gmail.com>
2024-06-28 01:52:23 +0530

    Created getUser API

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -76,4 +76,30 @@ const loginController = async (req, res) => {
   }
 };
 
-module.exports = { registerController, loginController };
+const getUserController = async(req, res) => {
+  try {
+    const user = await userModel.findOne({_id: req.body.userId});
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "User not found"
+      })
+    }
+    console.log(user)
+    return res.status(200).send({
+        success: true,
+        message: "User fetched successfully",
+        user
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in getUser API",
+      error,
+    });
+  }
+   
+}
+
+module.exports = { registerController, loginController, getUserController };
